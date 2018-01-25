5b1ee019ec4e439ce15bd4ebe947545f9fc6c8d5
wonghongngai <wonghongngai123@gmail.com>
2018-01-25 22:52:52 +0800

    Done logic for search, not sure whether it is useful though

--- tmsRead/SearchViewController.swift
+++ tmsRead/SearchViewController.swift
@@ -8,30 +8,31 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchBarDelegate {
     
-    @IBOutlet weak var tableView: UITableView!
-
+    @IBOutlet weak var searchResultsLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchResultsLabel.text = "Showing results for '\(searchText)'"
+        //print("searchText \(searchText)")
     }
-    */
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //self.searchResultsLabel.text = "Showing results for \((describing: searchBar.text))"
+        print("searchText \(String(describing: searchBar.text))")
+    }
 
 }
